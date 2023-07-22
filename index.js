9421985a8b2c8062d423be2b5e91316afb645d50
jahirul94 <jahirulislam41185@gmail.com>
2023-07-23 03:19:24 +0600

    reviews collection added

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-app.get('/' , (req , res) =>{
+app.get('/', (req, res) => {
     res.send('college server running')
 })
 
 
 
 // mongo start
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kri1sc7.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,27 +32,33 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        const collegeCollection = client.db("college-compass").collection("all-college");
+        const collegeCollection = client.db("college-compass").collection("colleges");
         const ApplicationCollection = client.db("college-compass").collection("Application");
+        const ReviewsCollection = client.db("college-compass").collection("reviews");
 
-        app.get('/all-college' , async (req , res) =>{
+        app.get('/all-college', async (req, res) => {
             const result = await collegeCollection.find().toArray();
             res.send(result);
         })
 
-        app.get('/myCollege' , async(req , res ) => {
+        app.get('/myCollege', async (req, res) => {
             const email = req.query.email;
-            const filter = {email: email}
-            const result = await ApplicationCollection.find(filter).toArray();
+            const query = { email: email }
+            const result = await ApplicationCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.post('/myCollege', async (req, res) => {
+            const data = req.body;
+            const result = await ApplicationCollection.insertOne(data);
+            res.send(result)
+        })
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await ReviewsCollection.insertOne(review);
             res.send(result);
         })
 
-       app.post('/myCollege' , async (req , res ) =>{
-         const data = req.body ;
-         const result = await ApplicationCollection.insertOne(data);
-         res.send(result)
-       })
-   
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
